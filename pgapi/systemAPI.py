3505c6b0264642c9ea800ec8a4564fdfd9f98547
Adrian Vondendriesch <adrian.vondendriesch@credativ.de>
2018-09-19 13:30:56 +0200

    Add additional system information to /system endpoint.
    This includes:
      * memory
      * swap
      * mountpoints
      * disk_usage
      * and more
    
    Moreover, this commit introduces a new endpoint /system/<section>
    which could be used query a specific section like e.g. "cpu_config".

--- pgapi/systemAPI.py
+++ pgapi/systemAPI.py
@@ -1,20 +1,22 @@
 from flask import jsonify, abort
 from flask_restful import Resource, reqparse
 import logging
+
 from pgapi.serverCommands import *
 
 class System(Resource):
 
-    def get(self):
+    def get(self, section=None):
         """Return global system informations.
 
            Example: GET /system
         """
-        logging.info("GET Request for System information")
+        logging.info("GET Request for System information, section=\"%s\"", section)
 
-        system_info = get_system_info()
+        system_info = get_system_info(section)
 
         return jsonify(system_info)
 
 def registerHandlers(api):
-    api.add_resource(System, '/system/')
+    api.add_resource(System, '/system/', endpoint="system")
+    api.add_resource(System, '/system/<string:section>/', endpoint="system_section")
